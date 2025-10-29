0bbfaec275e18695b0a10e07873693a0c09235ac
Valentin Petrovych <valentin.petrovich@postindustria.com>
2025-10-29 09:53:38 +0100

    feat: send rendering time calculated in JS code

--- src/postscribeRender.js
+++ src/postscribeRender.js
@@ -1,14 +1,14 @@
 import postscribe from 'postscribe';
 
 export function writeAdHtml(markup, ps = postscribe) {
-    dispatchGoogleAppEvent("puc_render_begin");
+    const renderStartTime = Date.now();
     // remove <?xml> and <!doctype> tags
     // https://github.com/prebid/prebid-universal-creative/issues/134
     markup = markup.replace(/\<(\?xml|(\!DOCTYPE[^\>\[]+(\[[^\]]+)?))+[^>]+\>/gi, '');
     ps(document.body, markup, {
         error: console.error,
         done: (stream) => {
-            dispatchGoogleAppEvent("puc_render_end");
+            dispatchGoogleAppEvent(renderStartTime);
         }
     });
 }
@@ -17,9 +17,10 @@ export function writeAdHtml(markup, ps = postscribe) {
  * Dispatch app event to Google Mobile Ads SDK.
  * https://developers.google.com/ad-manager/mobile-ads-sdk/ios/banner#app_events
  */
-function dispatchGoogleAppEvent(value) {
+function dispatchGoogleAppEvent(startTime) {
     try {
-        admob.events.dispatchAppEvent("prebid", value);
+        const renderTime = Date.now() - startTime;
+        admob.events.dispatchAppEvent("prebid_render_time", renderTime);
     } catch (e) {
     }
 }
